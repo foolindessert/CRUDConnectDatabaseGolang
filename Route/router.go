5388b2d1968e99aed85a02635ab6c3cb1a860c51
Sakuratan99 <gamemortal05@gmail.com>
2022-06-28 14:54:59 +0700

    Nambahin Route

---
+++ Route/router.go
@@ -0,0 +1,56 @@
+package route
+
+import (
+	handler "DATABASECRUD/Handler"
+	middleware "DATABASECRUD/Middleware"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+const PORT = ":8080"
+
+func RouterAll(db *sql.DB) {
+	// handler crud
+	r := mux.NewRouter()
+	userHandler := handler.NewUserHandler(db)
+	registerHandler := handler.UserRegisterHandler(db)
+	loginHandler := handler.UserLoginHandler(db)
+	r.HandleFunc("/users/register", registerHandler.RegisterUser)
+	r.HandleFunc("/users/login", loginHandler.LoginUser)
+	r.Handle("/users/{id}", middleware.AuthCekToken(http.HandlerFunc(userHandler.UsersHandler))).Methods("PUT")
+	r.Handle("/users", middleware.AuthCekToken(http.HandlerFunc(userHandler.UsersHandler))).Methods("DELETE")
+
+	//handler photo
+	photoHandler := handler.NewPhotoHandler(db)
+	r.Handle("/photos", middleware.AuthCekToken(http.HandlerFunc(photoHandler.PhotoHandler))).Methods("POST")
+	r.Handle("/photos", middleware.AuthCekToken(http.HandlerFunc(photoHandler.PhotoHandler))).Methods("GET")
+	r.Handle("/photos/{id}", middleware.AuthCekToken(http.HandlerFunc(photoHandler.PhotoHandler))).Methods("PUT")
+	r.Handle("/photos/{id}", middleware.AuthCekToken(http.HandlerFunc(photoHandler.PhotoHandler))).Methods("DELETE")
+	//handler comment
+	commentHandler := handler.NewCommentHandler(db)
+	r.Handle("/comments", middleware.AuthCekToken(http.HandlerFunc(commentHandler.CommentHandler))).Methods("POST")
+	r.Handle("/comments", middleware.AuthCekToken(http.HandlerFunc(commentHandler.CommentHandler))).Methods("GET")
+	r.Handle("/comments/{id}", middleware.AuthCekToken(http.HandlerFunc(commentHandler.CommentHandler))).Methods("PUT")
+	r.Handle("/comments/{id}", middleware.AuthCekToken(http.HandlerFunc(commentHandler.CommentHandler))).Methods("DELETE")
+	//handler socialMedia
+	socialmediaHandler := handler.NewSocialMediaHandler(db)
+	r.Handle("/socialmedias", middleware.AuthCekToken(http.HandlerFunc(socialmediaHandler.SocilaMediaHandler))).Methods("POST")
+	r.Handle("/socialmedias", middleware.AuthCekToken(http.HandlerFunc(socialmediaHandler.SocilaMediaHandler))).Methods("GET")
+	r.Handle("/socialmedias/{id}", middleware.AuthCekToken(http.HandlerFunc(socialmediaHandler.SocilaMediaHandler))).Methods("PUT")
+	r.Handle("/socialmedias/{id}", middleware.AuthCekToken(http.HandlerFunc(socialmediaHandler.SocilaMediaHandler))).Methods("DELETE")
+
+	fmt.Println("Now listening on port 0.0.0.0" + PORT)
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "0.0.0.0" + PORT,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
--- main.go
+++ main.go
@@ -4,25 +4,17 @@ import (
 	"database/sql"
 	_ "encoding/json"
 	"fmt"
-	"log"
-	"net/http"
-	"time"
 
-	handler "DATABASECRUD/Handler"
-	middleware "DATABASECRUD/Middleware"
+	route "DATABASECRUD/Route"
 	"DATABASECRUD/conf"
 
-	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
 
 var (
-	db *sql.DB
-
+	db  *sql.DB
 	err error
 )
-
-const PORT = ":8080"
 
 func main() {
 	// mmemastikan db connect atau tidak
@@ -36,45 +28,7 @@ func main() {
 		panic(err)
 	}
 	fmt.Println("Succesfully connected to database")
-
-	// handler crud
-	r := mux.NewRouter()
-	userHandler := handler.NewUserHandler(db)
-	registerHandler := handler.UserRegisterHandler(db)
-	loginHandler := handler.UserLoginHandler(db)
-	r.HandleFunc("/users/register", registerHandler.RegisterUser)
-	r.HandleFunc("/users/login", loginHandler.LoginUser)
-	r.Handle("/users/{id}", middleware.AuthCekToken(http.HandlerFunc(userHandler.UsersHandler))).Methods("PUT")
-	r.Handle("/users", middleware.AuthCekToken(http.HandlerFunc(userHandler.UsersHandler))).Methods("DELETE")
-
-	//handler photo
-	photoHandler := handler.NewPhotoHandler(db)
-	r.Handle("/photos", middleware.AuthCekToken(http.HandlerFunc(photoHandler.PhotoHandler))).Methods("POST")
-	r.Handle("/photos", middleware.AuthCekToken(http.HandlerFunc(photoHandler.PhotoHandler))).Methods("GET")
-	r.Handle("/photos/{id}", middleware.AuthCekToken(http.HandlerFunc(photoHandler.PhotoHandler))).Methods("PUT")
-	r.Handle("/photos/{id}", middleware.AuthCekToken(http.HandlerFunc(photoHandler.PhotoHandler))).Methods("DELETE")
-	//handler comment
-	commentHandler := handler.NewCommentHandler(db)
-	r.Handle("/comments", middleware.AuthCekToken(http.HandlerFunc(commentHandler.CommentHandler))).Methods("POST")
-	r.Handle("/comments", middleware.AuthCekToken(http.HandlerFunc(commentHandler.CommentHandler))).Methods("GET")
-	r.Handle("/comments/{id}", middleware.AuthCekToken(http.HandlerFunc(commentHandler.CommentHandler))).Methods("PUT")
-	r.Handle("/comments/{id}", middleware.AuthCekToken(http.HandlerFunc(commentHandler.CommentHandler))).Methods("DELETE")
-	//handler socialMedia
-	socialmediaHandler := handler.NewSocialMediaHandler(db)
-	r.Handle("/socialmedias", middleware.AuthCekToken(http.HandlerFunc(socialmediaHandler.SocilaMediaHandler))).Methods("POST")
-	r.Handle("/socialmedias", middleware.AuthCekToken(http.HandlerFunc(socialmediaHandler.SocilaMediaHandler))).Methods("GET")
-	r.Handle("/socialmedias/{id}", middleware.AuthCekToken(http.HandlerFunc(socialmediaHandler.SocilaMediaHandler))).Methods("PUT")
-	r.Handle("/socialmedias/{id}", middleware.AuthCekToken(http.HandlerFunc(socialmediaHandler.SocilaMediaHandler))).Methods("DELETE")
-
-	fmt.Println("Now listening on port 0.0.0.0" + PORT)
-	srv := &http.Server{
-		Handler:      r,
-		Addr:         "0.0.0.0" + PORT,
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
-	}
-
-	log.Fatal(srv.ListenAndServe())
+	route.RouterAll(db)
 }
 
 func ConnectDbPsql(host, user, password, name string, port int) string {
